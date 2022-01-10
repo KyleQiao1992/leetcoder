2d033e22d97e1bd5c17df6be44dff5b04e74369b
qiaoyi <qiaoyi@soulapp.cn>
2022-01-10 10:11:50 +0800

    up

---
+++ src/main/java/com/leetcoder/leetcoder/binaryTree/no98/isValidBST.java
@@ -0,0 +1,26 @@
+package com.leetcoder.leetcoder.binaryTree.no98;
+
+import com.leetcoder.leetcoder.binaryTree.TreeNode;
+
+/**
+ * @ Description :
+ * @ wiki:
+ * @ date: created in 5:52 下午 2022/1/7
+ **/
+public class isValidBST {
+
+    public boolean isValidBST(TreeNode root) {
+        return dfs(root, null, null);
+    }
+
+    private Boolean dfs(TreeNode root, TreeNode min, TreeNode max) {
+        // base case
+        if (root == null) return true;
+        // root.val max min BST
+        if (min != null && root.val <= min.val) return false;
+        if (max != null && root.val >= max.val) return false;
+        // root.val root.val
+        return dfs(root.left, min, root)
+                && dfs(root.right, root, max);
+    }
+}
